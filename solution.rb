99a6a9e8767e9271d0c7d72c56764d848827972b
raaynaldo <raaynaldo@hotmail.com>
2020-10-20 09:40:16 -0400

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,25 @@
 class Deck
+  attr_reader :cards
 
+  def initialize
+    @cards = []
+    ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"].each { |rank|
+      ["Hearts", "Clubs", "Diamonds", "Spades"].each { |suit|
+        @cards << Card.new(suit, rank)
+      }
+    }
+  end
+
+  def choose_card
+    self.cards.delete_at(rand(self.cards.length))
+  end
 end
 
 class Card
+  attr_reader :rank, :suit
 
+  def initialize(suit, rank)
+    @rank = rank
+    @suit = suit
+  end
 end
